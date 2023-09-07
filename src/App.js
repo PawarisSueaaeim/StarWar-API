a4c6880e4059dc778913e8534d531aa909ed460c
PawarisSueaaeim <pawaris.sueaaeim@gmail.com>
2023-09-07 18:11:53 +0700

    post request

--- src/App.js
+++ src/App.js
@@ -1,43 +1,83 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React, { useState, useEffect, useCallback } from 'react';
 
-import MoviesList from "./components/MoviesList";
-import "./App.css";
+import MoviesList from './components/MoviesList';
+import AddMovie from './components/AddMovies';
+import './App.css';
 
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-  const fetchDummyMovies = async () => {
+  const fetchMoviesHandler = useCallback(async () => {
+    setIsLoading(true);
+    setError(null);
     try {
-      setIsLoading(true);
-      const response = await axios.get("https://swapi.dev/api/films/");
-      const tranfromData = response.data.results.map(data => {
+      const response = await fetch('https://react-http-starwar-ec2bd-default-rtdb.asia-southeast1.firebasedatabase.app/movies.json');
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+
+      const data = await response.json();
+
+      const transformedMovies = data.results.map((movieData) => {
         return {
-          id: data.episode_id,
-          title: data.title,
-          openingText: data.opening_crawl,
-          releaseDate: data.release_date
-        }
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        };
       });
-      setMovies(tranfromData);
+      setMovies(transformedMovies);
     } catch (error) {
-      console.log(error);
+      setError(error.message);
+    }
+    setIsLoading(false);
+  }, []);
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
+
+  async function addMovieHandler(movie) {
+    try {
+      const response = await fetch('https://react-http-starwar-ec2bd-default-rtdb.asia-southeast1.firebasedatabase.app/', {
+        method: 'POST',
+        body: JSON.stringify(movie),
+        headers: { 'Content-Type': 'application/json' },
+      })
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      setError(error.message)
     } finally {
       setIsLoading(false);
     }
-  };
+  }
+
+  let content = <p>Found no movies.</p>;
+
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+
+  if (error) {
+    content = <p>{error}</p>;
+  }
+
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  }
 
   return (
     <React.Fragment>
       <section>
-        <button onClick={fetchDummyMovies}>Fetch Movies</button>
+        <AddMovie onAddMovie={addMovieHandler} />
       </section>
       <section>
-        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-        {!isLoading && movies.length === 0 && <p>Not Found Movies</p>}
-        {isLoading && <p>Loading...</p>}
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
